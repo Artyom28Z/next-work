675e9a02258a9b2fffcc6502646b57f83677e777
artyom <artemaryj36119@gmail.com>
2025-08-30 12:01:49 +0300

    1 dz v2

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from main.models import Product, Category
+
 # Register your models here.
+
+#admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'category', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('first_name', 'category')
+
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
--- main/models.py
+++ main/models.py
@@ -1,4 +1,36 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True} #Для необязательных полей
 
+
+class Product(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Название продукта',
+                                  help_text='Введите название продукта')
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, verbose_name='Название категории',
+                                  help_text='Укажите название категории', related_name="products", **NULLABLE)
+    avatar = models.ImageField(upload_to='products/', verbose_name='Аватвр',
+                               help_text='Загрузите фото продукта', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Есть в продаже')
+    date_born = models.DateField(verbose_name='Дата публикации', help_text='Укажите дату публикации продукта',
+                                 **NULLABLE)
+
+    def __str__(self):
+        return f'{self.first_name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('first_name', 'category',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название категории',
+                            help_text='Введите название категории')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
